54901e5757aa63022519f9f455fd3a12a1d14426
Nikita Tysiachnyi <nikita.tysiachnyi@extreme-idea.com>
2020-12-12 01:50:17 +0200

    Updated all method, added material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'custom-calculator';
+  value = 0;
+  result: string | number[] = 'The result will be displayed here 🚀 🚀 🚀';
+
+  constructor(private snackBar: MatSnackBar) {}
+
+  calculate(value: number): void {
+    if (value === 0) {
+      this.snackBar.open(`${value} cannot be calculated`, 'Close', {
+        duration: 3000,
+      });
+    } else {
+      const maxNum = 4950;
+
+      const allData = Array.from(
+        {length: Math.floor(value / maxNum)},
+        () => maxNum
+      );
+      const leftSide = value % maxNum;
+
+      if (leftSide !== 0) {
+        allData.push(leftSide);
+      }
+      this.result = allData.toString().split(',').join(', ');
+      this.snackBar.open(`${value} successfully calculated`, 'Close', {
+        duration: 3000,
+      });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material/card';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTabsModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatSnackBarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
