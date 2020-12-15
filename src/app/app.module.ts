0dbe702489bf93a4d363bfa192360cd74834fbcb
Nikita Tysiachnyi <nikita.tysiachnyi@extreme-idea.com>
2020-12-16 00:20:53 +0200

    added functional button, added modal, and interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,24 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTabsModule} from '@angular/material/tabs';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatCardModule} from '@angular/material/card';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-import { MainComponent } from './components/main/main.component';
-import { ExportComponent } from './components/export/export.component';
+import {MainComponent} from './components/main/main.component';
+import {ExportComponent} from './components/export/export.component';
+import {ExportDialogComponent} from './components/export-dialog/export-dialog.component';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, ExportComponent],
+  declarations: [
+    AppComponent,
+    MainComponent,
+    ExportComponent,
+    ExportDialogComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -31,6 +38,8 @@ import { ExportComponent } from './components/export/export.component';
     FlexLayoutModule,
     MatCardModule,
     MatSnackBarModule,
+    ReactiveFormsModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/export/export.component.ts
+++ src/app/components/export/export.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {ExportDialogComponent} from '../export-dialog/export-dialog.component';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-export',
@@ -6,11 +9,46 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./export.component.scss'],
 })
 export class ExportComponent implements OnInit {
-  value = 0;
-  name = '';
-  card = '';
+  usersForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder, public dialog: MatDialog) {
+    this.usersForm = this.fb.group({
+      users: this.fb.array([
+        this.fb.group({
+          creditCard: new FormControl(''),
+          sum: new FormControl(0),
+          name: new FormControl(''),
+        }),
+      ]),
+    });
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.users);
+  }
+
+  get users(): Partial<FormArray> {
+    return this.usersForm.get('users');
+  }
+
+  addNewUser(): void {
+    this.users.push(
+      this.fb.group({
+        creditCard: new FormControl(''),
+        sum: new FormControl(0),
+        name: new FormControl(''),
+      })
+    );
+  }
+
+  AddMoreUsers(): void {
+    const dialogRef = this.dialog.open(ExportDialogComponent, {
+      width: '300px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result && result > 0) {
+      }
+    });
+  }
 }
